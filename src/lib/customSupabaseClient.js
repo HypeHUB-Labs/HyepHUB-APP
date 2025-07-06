27aa54bbdcd570cc06fb2031ec460a8a48d5a640
HypeHUB <hypehub.mail@gmail.com>
2025-07-07 00:25:31 +0700

    Update customSupabaseClient.js

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://loikkjwyzbeeinbasvnq.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvaWtrand5emJlZWluYmFzdm5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NTgwNTMsImV4cCI6MjA2NTMzNDA1M30.vPnwJkD2U2pO-x7d2VGtvWjmSN4yOrvTjHIKkKxPZvE';
+const supabaseAnonKey = 'Add Yours Key';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
